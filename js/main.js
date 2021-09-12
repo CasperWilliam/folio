33ca3116ef350d6ba8948c2bba1717c57a2cdc7d
Lucas Siefert <theapemachine@Lucass-Air.attlocal.net>
2021-09-12 15:13:33 -0400

    carousel in progress

--- js/main.js
+++ js/main.js
@@ -26,6 +26,207 @@ const filterLink = document.querySelectorAll(dataFilter);
 const portfolioItems = document.querySelectorAll(portfolioData);
 const searchBox = document.querySelector("#search");
 
+/* portfolio card object*/
+console.log(document.querySelectorAll(portfolioData))
+const portfolioCards = [
+  {
+    dataItem: "web",
+    dataOpen: "web-1",
+    title: "Web Development",
+    detail: "Food Website",
+    img: "./assets/portfolio-1.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "web",
+    dataOpen: "web-2",
+    title: "Web Development",
+    detail: "Skate Website",
+    img: "./assets/portfolio-2.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "web",
+    dataOpen: "web-3",
+    title: "Web Development",
+    detail: "Fitness Website",
+    img: "./assets/portfolio-3.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "app",
+    dataOpen: "app-1",
+    title: "App Development",
+    detail: "Game App",
+    img: "./assets/portfolio-5.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "app",
+    dataOpen: "app-2",
+    title: "App Development",
+    detail: "Gambling App",
+    img: "./assets/portfolio-6.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "app",
+    dataOpen: "app-3",
+    title: "App Development",
+    detail: "Finance App",
+    img: "./assets/portfolio-7.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "ui",
+    dataOpen: "ui-1",
+    title: "UI Design",
+    detail: "Intricate Design",
+    img: "./assets/portfolio-4.jpg",
+    alt: "portfolio",
+  },
+  {
+    dataItem: "ui",
+    dataOpen: "ui-2",
+    title: "UI Design",
+    detail: "Fantastic Design",
+    img: "./assets/portfolio-8.jpg",
+    alt: "portfolio",
+  },
+];
+
+// Portfolio card loop
+
+const cardsParent = document.getElementById("cardBody");
+
+portfolioCards.forEach((item) => {
+  const content = `<div class="portfolio-card" data-item=${item.dataItem} data-open=${item.dataOpen}>
+      <div class="card-body">
+        <img src=${item.img} alt=${item.alt} />
+        <div class="card-popup-box">
+          <div>${item.title}</div>
+          <h3>${item.detail}</h3>
+        </div>
+      </div>
+    </div>`;
+
+  cardsParent.innerHTML += content;
+});
+
+// Modal pop-ups
+
+const modalCards = [
+  {
+    id: "web-1",
+    header: "Web Project I",
+    img: "./assets/portfolio-1.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "web-2",
+    header: "Web Project II",
+    img: "./assets/portfolio-2.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "web-3",
+    header: "Web Project III",
+    img: "./assets/portfolio-3.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "app-1",
+    header: "App Project I",
+    img: "./assets/portfolio-5.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "app-2",
+    header: "App Project II",
+    img: "./assets/portfolio-6.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "app-3",
+    header: "App Project III",
+    img: "./assets/portfolio-7.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "ui-1",
+    header: "UI Project I",
+    img: "./assets/portfolio-4.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+  {
+    id: "ui-2",
+    header: "UI Project II",
+    img: "./assets/portfolio-8.jpg",
+    text1: "My first awesome website",
+    text2:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam atque quibusdam fuga.",
+    text3:
+      "Perspiciatis vel placeat veniam minima fugiat alias beatae, accusantium, velit magnam suscipit accusamus ipsam earum odit. Ducimus?",
+  },
+];
+
+const modalParent = document.getElementById("modalMain");
+
+modalCards.forEach((item) => {
+  const content = `<div id="${item.id}" class="modal" data-animation="slideInOutTop">
+    <div class="modal-dialog">
+      <header class="modal-header">
+        <h3>${item.header}</h3>
+        <i class="fas fa-times" data-close></i>
+      </header>
+      <div class="modal-body">
+        <div class="img-wrapper">
+          <img src="${item.img}" alt="portfolio image" />
+        </div>
+        <div class="text-wrapper">
+          <p><strong>${item.text1}</strong></p>
+          <p>
+            ${item.text2}
+          </p>
+          <p>
+            ${item.text3}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  modalParent.innerHTML += content;
+});
+
 const setActive = (elm, selector) => {
   if (document.querySelector(`${selector}.${active}`) !== null) {
     document.querySelector(`${selector}.${active}`).classList.remove(active);
@@ -85,22 +286,6 @@ searchBox.addEventListener("keyup", (e) => {
   });
 });
 
-for (const link of filterLink) {
-  link.addEventListener("click", function () {
-    setActive(link, ".filter-link");
-    const filter = this.dataset.filter;
-    portfolioItems.forEach((card) => {
-      if (filter === "all") {
-        card.style.display = "block";
-      } else if (card.dataset.item === filter) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
-  });
-}
-
 /* modal */
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
@@ -122,15 +307,30 @@ for (const elm of closeModal) {
 //modal
 
 document.addEventListener("click", (e) => {
-  console.log(e.target, document.querySelector(".modal.is-visible"));
+ if (e.target.className && e.target.getAttribute('class').includes('filter-link')) {
+  setActive(e.target, ".filter-link");
+  const filter = e.target.dataset.filter;
+  document.querySelectorAll(portfolioData).forEach((card) => {
+    if (filter === "all") {
+      card.style.display = "block";
+    } else if (card.dataset.item === filter) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  })
+}
+
+  //console.log(e.target, document.querySelector(".modal.is-visible"));
   if (e.target === document.querySelector(".modal.is-visible")) {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
 
-document.addEventListener('keyup', (e) => {
-  console.log(e.key);
-  if (e.key === 'Escape') {
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
+
+console.log(document.querySelectorAll(portfolioData))
